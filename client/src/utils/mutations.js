7f00c6cfd26cbe4521601357bdea24aa09d4303e
Jared Staples <jstaplesmusic@gmail.com>
2023-02-05 13:42:03 -0500

    refactor(queries.js mutations.js): created gql queries and mutations for the front-end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        _id
+        username
+      }
+      token
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      user {
+        username
+        _id
+      }
+      token
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      username
+      savedBooks {
+        title
+        authors
+        bookId
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+      }
+    }
+  }
+`;
