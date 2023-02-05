f3ae57fc5429c7059dabce6920928ca1c2299bd2
Jared Staples <jstaplesmusic@gmail.com>
2023-02-05 13:08:52 -0500

    refactor(resolvers.js): created resolvers to be used by apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const reslovers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect email/password");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect email/password");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, BookInput, context) => {
+      if (context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { savedBooks: BookInput },
+          },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError("Must be logged in to save books");
+    },
+    removeBook: async (parent, bookId, context) => {
+      if (context.user) {
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError("Must be logged in to save books");
+    },
+  },
+};
